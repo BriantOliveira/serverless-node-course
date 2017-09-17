b19646688829d1b5009455a94a4b74342f75c45d
Loren Stewart <l.s.stewart111@gmail.com>
2017-09-17 08:41:05 -0700

    first commit

---
+++ 4_s3_notifications/8_send_email/mailer/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  user: "l.s.stewart111@gmail.com",
+  password: "dfkciqzgwiyxnqfs",
+  from: "l.s.stewart111@gmail.com",
+  to: ["loren@codemore.io"]
+};
+
+// module.exports = {
+//   user: "",
+//   password: ""
+//   from: "",
+//   to: ""
+// };
